36225ddd6bbc2f9d7a3268236edf7d6f80729ad3
Shivani21-hude <shivani21hude@gmail.com>
2023-01-13 21:09:42 +0530

    Flipcoin Problem

---
+++ BasicCoreProblem/Flipcoin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblem
+{
+    internal class Flipcoin
+    {
+        public static void PerHeadTail()
+        {
+            Console.WriteLine("Enter the number for flipcoin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int CountHead = 0;
+            int CountTail = 0;
+            for (int i = 0; i < n; i++)
+            {
+
+                Random random = new Random();
+                int num = random.Next(2);
+                if (num == 0)
+                {
+                    Console.WriteLine("tail");
+                    CountHead++;
+                }
+                else
+                {
+                    Console.WriteLine("head");
+                    CountTail++;
+                }
+
+            }
+            Console.WriteLine("number of Heads :" + CountHead);
+            Console.WriteLine("number of tails : " + CountTail);
+            double Head = (CountHead / (double)n) * 100;
+            double Tail = (CountTail / (double)n) * 100;
+            Console.WriteLine("Percentage of head : " + Head);
+            Console.WriteLine("percentage of tail : " + Tail);
+        }
+    }
+}
---
+++ BasicCoreProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BasicCoreProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flipcoin.PerHeadTail();
+           
+        }
+    }
+}
